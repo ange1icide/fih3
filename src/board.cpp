8b2c87b5996761ec64d636888e23ba6d8d2a83a5
CrystalVoid0 <crystal_spam09@proton.me>
2026-04-26 16:34:45 -0700

    Did something

---
+++ src/board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by crystal on 4/26/26.
+//
+
+#include "board.h"
---
+++ src/include/board.h
@@ -0,0 +1,13 @@
+//
+// Created by crystal on 4/26/26.
+//
+
+#ifndef FIH3_BOARD_H
+#define FIH3_BOARD_H
+
+
+class board {
+};
+
+
+#endif //FIH3_BOARD_H
